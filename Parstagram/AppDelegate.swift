ee0489fc3e9ea42b953fd67728c016f471845836
jpham35 <49501108+jpham35@users.noreply.github.com>
2020-10-24 01:05:10 -0500

    added login functionality, camera and photo upload capability

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -6,14 +6,22 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = "i6dsp6SIm4k9zN4VWWcCA68qvCmYBZdiVvyU6L3v"
+                $0.clientKey = "hq7DrEeAZjGHsMK6k1YITXAa3IUiBM1rJ3WDcOka"
+                $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+
         return true
     }
 
